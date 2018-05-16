717abaf75ff81e87f17221126f1f9ca8ae0714fe
Nicolas <jnsastoquee@unal.edu.co>
2018-05-16 15:46:47 -0500

    agregada columna unit_id

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ type Todayslesson struct {
 	ID          string `json:"id,omitempty"`
 	Name        string `json:"name,omitempty"`
 	Description string `json:"description,omitempty"`
+	Unit_id			string `json:"unit_id,omitempty"`
 }
 
 var todayslessons []Todayslesson
@@ -30,56 +31,23 @@ func GetTodayslesson(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(&Todayslesson{})
 }
-/*
-func CreateTodayslesson(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-    var todayslesson Todayslesson
-    _ = json.NewDecoder(r.Body).Decode(&todayslesson)
-    todayslesson.ID = params["id"]
-    todayslessons = append(todayslessons, todayslesson)
-    json.NewEncoder(w).Encode(todayslessons)
-}
 
-func DeleteTodayslesson(w http.ResponseWriter, r *http.Request) {
-	/*
-	params := mux.Vars(r)
-    for index, item := range todayslessons {
-        //if item.ID == params["id"] {
-    	if item.ID == id {
-            todayslessons = append(todayslessons[:index], todayslessons[index+1]...)
-            break
-    	}
- 		json.NewEncoder(w).Encode(todayslessons)
-	}
-
-	*/
-	/*
-	for i, t := range todos {
-        if t.Id == id {
-            todos = append(todos[:i], todos[i+1:]...)
-            return nil
-        }
-    }
-    return fmt.Errorf("Could not find Todo with id of %d to delete", id)
-    
-}
-*/
 // our main function
 func main() {
 	router := mux.NewRouter()
 
 	//data
-	todayslessons = append(todayslessons, Todayslesson{ID: "1", Name: "A blessing in disguise", Description: "a good thing that seemed bad at first"})
-	todayslessons = append(todayslessons, Todayslesson{ID: "2", Name: "A dime a dozen", Description: "Something common"})
-	todayslessons = append(todayslessons, Todayslesson{ID: "3", Name: "Beat around the bush", Description: "Avoid saying what you mean, usually because it is uncomfortable"})
-	
+	todayslessons = append(todayslessons, Todayslesson{ID: "1", Name: "A blessing in disguise", Description: "a good thing that seemed bad at first", Unit_id: "1"})
+	todayslessons = append(todayslessons, Todayslesson{ID: "2", Name: "A dime a dozen", Description: "Something common", Unit_id: "2"})
+	todayslessons = append(todayslessons, Todayslesson{ID: "3", Name: "Beat around the bush", Description: "Avoid saying what you mean, usually because it is uncomfortable", Unit_id: "3"})
+
 	router.HandleFunc("/todayslessons", GetTodayslessons).Methods("GET")
 	router.HandleFunc("/todayslessons/", GetTodayslessons).Methods("GET")
 	router.HandleFunc("/todayslessons/{id}", GetTodayslesson).Methods("GET")
 	//router.HandleFunc("/todayslessons/{id}", CreateTodayslesson).Methods("POST")
 	//router.HandleFunc("/todayslessons/{id}", DeleteTodayslesson).Methods("DELETE")
 	fmt.Printf("Server Running")
-
+	
 	//log.Fatal(http.ListenAndServe(":4002", router))
 	err := http.ListenAndServe(":4002", router) // set listen port
 	if err != nil {
